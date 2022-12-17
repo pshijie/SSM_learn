b9e6edc99c39b5ac89f5909135454e645f678278
psj <729589165@qq.com>
2022-12-17 20:33:27 +0800

    添加Maven01-04完整代码

---
+++ Maven_02_ssm/src/main/java/com/psj/config/SpringMvcConfig.java
@@ -0,0 +1,11 @@
+package com.psj.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan({"com.psj.controller","com.psj.config"})
+@EnableWebMvc
+public class SpringMvcConfig {
+}
